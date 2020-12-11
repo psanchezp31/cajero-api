e58252fd9d1bab01e54d3faffdd4310774ccd477
Paula Sanchez <paula.sanchez1031@gmail.com>
2020-12-11 17:54:41 -0500

    Agregando link de heroku en origins/main.py

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from fastapi.middleware.cors import CORSMiddleware
 origins = [
 "http://localhost.tiangolo.com", "https://localhost.tiangolo.com",
 "http://localhost", "http://localhost:8080",
+"https://cajero-app-paula.herokuapp.com"
 ]
 api.add_middleware(
     CORSMiddleware, allow_origins=origins,
